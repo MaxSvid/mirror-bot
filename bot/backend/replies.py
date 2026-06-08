ea62924f6f67e2e5c0cfe1ec2f7b913d7c81f10f
MaksSvid <sviderskiymaks04@gmail.com>
2026-06-09 00:35:36 +0100

    replies idea

---
+++ bot/backend/replies.py
@@ -0,0 +1,44 @@
+from aiogram.types import Message
+from aiogram import F, Router
+import random
+import json
+
+router = Router()
+
+def load_replies() -> dict:
+    try:
+        with open("bot/backend/links.json", "r", encoding="utf-8") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        print("Error: The file 'links.json' was not found.")
+        return {}
+    except OSError as e:
+        print(f"Error: OS error: {e}")
+        return {}
+
+# Funny reply on окей message
+@router.message(F.text.lower() == "окей")
+async def reply_okei(message: Message) -> None:
+    replies = load_replies()
+    await message.reply(random.choice(replies["ok_replies"]))
+
+
+# TikTok Links reply
+@router.message(F.text.contains("tiktok.com"))
+async def reply_tiktok(message: Message) -> None:
+    replies = load_replies()
+    await message.reply(random.choice(replies["tiktok_replies"]))
+
+
+# YouTube Links reply
+@router.message(F.text.contains("youtube.com/shorts"))
+async def reply_youtube(message: Message) -> None:
+    replies = load_replies()
+    await message.reply(random.choice(replies["youtube_replies"]))
+
+
+# Instagram Reels Links reply
+@router.message(F.text.contains("instagram.com/reel/"))
+async def reply_instagram(message: Message) -> None:
+    replies = load_replies()
+    await message.reply(random.choice(replies["insta_replies"]))
--- main.py
+++ main.py
@@ -1,6 +1,30 @@
-def main():
-    print("Hello from telegram-group-bot!")
+from aiogram import Bot, Dispatcher
+from dotenv import load_dotenv
 
+from bot.replies import router as replies_router
+from bot.handlers import router as main_router
+from bot.config import settings
 
-if __name__ == "__main__":
-    main()
+import asyncio
+import logging
+import sys
+
+load_dotenv()
+
+# Polling bot
+async def main() -> None:
+    bot = Bot(token=settings.BOT_TOKEN, parse_mode = "HTML")
+    dp = Dispatcher()
+
+    dp.include_router(main_router)
+    dp.include_router(replies_router)
+
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    try:
+        logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+        logging.info("Starting running bot...")
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        logging.info("Bot turned off...")
