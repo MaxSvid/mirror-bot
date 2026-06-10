a52b99b5d68fae753f25bb48e871d75a5863608f
MaksSvid <sviderskiymaks04@gmail.com>
2026-06-10 15:49:47 +0100

    1st handlers

---
+++ bot/handlers.py
@@ -0,0 +1,23 @@
+from aiogram import F, Router
+from aiogram.filters import Command, CommandStart
+from aiogram.fsm.context import FSMContext
+from aiogram.types import CallbackQuery, Message
+
+import logging
+import bot.keyboard as kyeboard
+
+router = Router()
+
+MENU_MESSAGE = "Hello! I'm Mirror bot 🤖"
+
+@router.message(CommandStart())
+async def cmd_start(message: Message) -> None:
+    logging.info(f"user_id={message.from_user.id}")
+    await message.answer(MENU_MESSAGE, reply_markup=keyboard.main)
+
+
+# --- Settings ---
+@router.callback_query(F.data == "menu_settings")
+async def settings_menu(callback: CallbackQuery) -> None:
+    await callback.message.edit_text("Settings", reply_markup=keyboard.settings_options)
+    await callback.answer()
